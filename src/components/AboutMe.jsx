3a70c3a1c278dbb0fccc72a2063c1eaf87901d6e
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-02 18:37:15 -0800

    AboutMe file

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <div className="about-me">
+        <h2>About Me</h2>
+        <div className="bio">
+          <img src="path_to_image" alt="Developer Avatar" />
+          <p>Short bio or description of the developer goes here...</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
