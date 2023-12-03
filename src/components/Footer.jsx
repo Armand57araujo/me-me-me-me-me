e9203c0acc4f51fe5785bba38a31a8ae654bb4db
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-12-02 18:49:02 -0800

    footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/developer" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        <a href="https://linkedin.com/developer" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+        <a href="https://stackoverflow.com/developer" target="_blank" rel="noopener noreferrer">
+          Stack Overflow
+        </a>
+        {/* Add more social links */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
